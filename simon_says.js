c81d4394ab529e5c6b38717b218b284ba988d770
victoriak94 <vkamila6@gmail.com>
2019-01-13 00:33:24 -0500

    Write functions according to tests. Complete assignment

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,25 @@
-module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
+module.exports = {echo, shout, repeat, startOfWord, firstWord};
+
+function echo(word) {
+  return `${word}`;
+};
+
+function shout(word) {
+  let wordUpperCase = word.toUpperCase();
+  return `${wordUpperCase}`;
+};
+
+function repeat(word, numberRepeat) {
+  wordWithSpace = word + ' ';
+  wordRepeated = wordWithSpace.repeat(numberRepeat);
+  return wordRepeated.split('').slice(0, -1).join('');
+};
+
+function startOfWord(word, numberLettersReturned) {
+  return word.split('').slice(0, numberLettersReturned).join('');
+};
+
+function firstWord(message) {
+  let messageArray = message.split(' ');
+  return messageArray[0];
+};
